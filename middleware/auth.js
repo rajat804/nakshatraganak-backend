6422e38e7c4ca58aa2ab8ed2b2519846656b1c21
Rajat <rajat804@gmail.com>
2026-06-04 14:28:42 +0530

    auth js update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,23 +4,19 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
-      console.error(error);
-      return res.status(401).json({ msg: 'Not authorized, token failed' });
+      return res.status(401).json({ success: false, message: 'Not authorized' });
     }
   }
 
   if (!token) {
-    return res.status(401).json({ msg: 'Not authorized, no token' });
+    return res.status(401).json({ success: false, message: 'Not authorized, no token' });
   }
 };
 
