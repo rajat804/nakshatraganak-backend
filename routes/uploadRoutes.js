5cd4cac243f73c0646437e5183af06116abbec06
Rajat <rajat804@gmail.com>
2026-06-08 13:46:39 +0530

    upload routes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { protectAdmin } = require('../middleware/adminAuth');
 const { 
   uploadSingleImage, 
   uploadMultipleImages, 
   deleteImage,
-  upload 
+  uploadServiceImage,
+  upload,
+  uploadService
 } = require('../controllers/uploadController');
-const { protectAdmin } = require('../middleware/adminAuth');
 
-// Admin only routes
+// General image uploads (for products, blogs, etc.)
 router.post('/single', protectAdmin, upload.single('image'), uploadSingleImage);
 router.post('/multiple', protectAdmin, upload.array('images', 10), uploadMultipleImages);
 router.delete('/image', protectAdmin, deleteImage);
+
+// Service image upload
+router.post('/service-image', protectAdmin, uploadService.single('image'), uploadServiceImage);
+router.delete('/service-image', protectAdmin, deleteImage);
 
 module.exports = router;
