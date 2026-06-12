a6b9e444c82cc57a4205ab6e744a225c3fb63325
Rajat <rajat804@gmail.com>
2026-06-12 14:27:46 +0530

    user udpate

--- models/User.js
+++ models/User.js
@@ -45,14 +45,29 @@ const userSchema = new mongoose.Schema({
   }]
 }, { timestamps: true });
 
+// ✅ FIXED: Pre-save hook to hash password
 userSchema.pre('save', async function (next) {
+  // Only hash if password is modified
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
+// ✅ FIXED: Compare password method
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  if (!enteredPassword || !this.password) {
+    console.log('Missing password data');
+    return false;
+  }
+  const isMatch = await bcrypt.compare(enteredPassword, this.password);
+  console.log(`Password comparison result: ${isMatch}`);
+  return isMatch;
 };
 
 module.exports = mongoose.model('User', userSchema);
